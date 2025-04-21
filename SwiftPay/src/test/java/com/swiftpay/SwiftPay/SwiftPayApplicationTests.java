c32c7c09854d2d480c2680a60e18ee3b90760b08
Jyotiraditya <jyotiradityaraghuvanshi75@gmail.com>
2025-04-21 19:08:23 +0530

    Project setupped successfully

---
+++ SwiftPay/src/test/java/com/swiftpay/SwiftPay/SwiftPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftpay.SwiftPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
